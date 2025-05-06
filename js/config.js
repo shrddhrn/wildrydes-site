917441032abce8248f329eb611569182594239bc
shrddhrn <shraddharana979@gmail.com>
2025-05-06 23:16:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_Fw8Mjmudh', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'del91op3c0n176d7u45v21j26', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-1
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
